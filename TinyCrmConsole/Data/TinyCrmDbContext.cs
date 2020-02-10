665ce9d17ff07a32e4bbc34e0eba94c01d891235
konsta-katsanou <58183830+konsta-katsanou@users.noreply.github.com>
2020-02-10 14:10:20 +0200

    initial commit

---
+++ TinyCrmConsole/Data/TinyCrmDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TinyCrm.Core.Data
+{
+    public class TinyCrmDbContext : DbContext
+    {
+        private readonly string connectionString_;
+
+        public TinyCrmDbContext()
+        {
+            connectionString_ = "Server = DESKTOP-TJ30A6D\\BASI17; Database = ecommerce; Integrated Security=SSPI  ; Persist Security Info=False;";
+        }
+
+        public TinyCrmDbContext(string connString)
+        {
+            connectionString_ = connString; 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<Model.Customer>()
+                .ToTable("Customer", "core");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(connectionString_);
+        }
+    }
+}
---
+++ TinyCrmConsole/Model/Customer.cs
@@ -0,0 +1,42 @@
+﻿namespace TinyCrm.Core.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Lastname { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string VatNumber { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Firstname { get; set; }
+        ///
+
+        ///
+        public int Age { get; set; }
+    }
+}
